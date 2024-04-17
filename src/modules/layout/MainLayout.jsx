9b370ef51b9d56d02f1937de3cc2f939c0a9aea6
Faiz <alfian@smkn1kandeman.sch.id>
2024-04-17 08:23:01 +0700

    change welcome text

--- src/modules/layout/MainLayout.jsx
+++ src/modules/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ const MainLayout = () => {
   return (
     <div className="text-center mt-[400px]">
       <h1 className='text-5xl font-light'>Hello World</h1>
-      <p className='text-3xl font-extralight'>Welcome to KSU Teknika Mandiri App</p>
+      <p className='text-3xl font-extralight'>Welcome to Seattle International Language School App App</p>
     </div>
   )
 }
