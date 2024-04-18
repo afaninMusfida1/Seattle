e402362fdc2a4b505cbb9b96a35d0018b852e27f
ZelikaSheril <zelika227123@student.smkn1kandeman.sch.id>
2024-04-18 11:15:39 +0700

    New

--- src/modules/layout/MainLayout.jsx
+++ src/modules/layout/MainLayout.jsx
@@ -1,10 +1,28 @@
+import React from 'react';
 const MainLayout = () => {
-  return (
-    <div className="text-center mt-[400px]">
-      <h1 className='text-5xl font-light'>Hello World</h1>
-      <p className='text-3xl font-extralight'>Welcome to Seattle International Language School App App</p>
-    </div>
-  )
-}
-
-export default MainLayout
+    return (
+      <div className="layout bg-gray-100">
+        <header className="bg-white shadow">
+          <div className="container mx-auto py-4 px-4">
+            
+            <h1 className="text-2xl font-bold">Seattle</h1>
+          </div>
+        </header>
+        <main className="container mx-auto py-4 px-4">
+          <section className="mb-4">
+            <p className="text-xl font-poppins mb-2">Overview</p>
+          </section>
+          <section className="mb-4">
+            <h2 className="text-xl font-bold mb-2">Guru</h2>
+          </section>
+          <section className="mb-4">
+            <h2 className="text-xl font-bold mb-2">Siswa</h2>
+          </section>
+          <section className="mb-4">
+            <h2 className="text-xl font-bold mb-2">Rekap</h2>
+          </section>
+        </main>
+      </div>
+    );
+};
+export default MainLayout;
