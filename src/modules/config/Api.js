9ee587e00e3a8dbfa358fc8ef54c695d37306414
afaninmusfida <afanin227089@student.smkn1kandeman.sch.id>
2024-04-18 10:59:47 +0700

    tampilan login selesai

---
+++ src/modules/config/Api.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+// import { http } from "./http";
+
+// export const handleLogin = async (username, password) => {
+//   const apiLogin = await axios
+//     .post(http + "auth/login", {
+//       username: username,
+//       password: password,
+//     })
+//     .then((response) => {
+//       return response;
+//     })
+//     .catch((err) => {
+//       return err.response;
+//     });
+//   return apiLogin;
+// };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from '../modules/layout/MainLayout'
+import Login from '../modules/auth/Login'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainLayout />} />
-        {/* write other routes here */}
+        <Route path='/' element={<Navigate to="/login"/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
