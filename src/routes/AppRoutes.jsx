d88763b850cdb7e5ceb358feef16674760076137
afaninmusfida <afanin227089@student.smkn1kandeman.sch.id>
2024-04-19 08:30:19 +0700

    navbar sidebar

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from '../modules/layout/MainLayout';
 import Login from '../modules/auth/Login';
-import Sidebar from '../modules/layout/Sidebar';
-import { Navbar } from '@material-tailwind/react';
 
 const AppRoutes = () => {
   return (
