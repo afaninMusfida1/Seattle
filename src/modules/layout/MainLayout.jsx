dc4115cb14cfdc383e4012df15668322d76e8ec4
Anaismatul <ana227092@student.smkn1kandeman.sch.id>
2024-04-19 08:24:02 +0700

    New Projek

--- src/modules/layout/MainLayout.jsx
+++ src/modules/layout/MainLayout.jsx
@@ -1,8 +1,12 @@
+import Dashboard from "../Dasboard/dashboard"
+
 const MainLayout = () => {
   return (
-    <div className="text-center mt-[400px]">
-      <h1 className='text-5xl font-light'>Hello World</h1>
-      <p className='text-3xl font-extralight'>Welcome to Seattle International Language School App App</p>
+    // <div className="text-center mt-[400px]">
+    <div>
+      {/* <h1 className='text-5xl font-light'>Hello World</h1>
+      <p className='text-3xl font-extralight'>Welcome to Seattle International Language School App App</p> */}
+      <Dashboard/>
     </div>
   )
 }
