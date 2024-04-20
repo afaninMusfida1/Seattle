e28bb964eec5b5a9535be4449b37fd8c3f400476
Anaismatul <ana227092@student.smkn1kandeman.sch.id>
2024-04-20 14:05:49 +0700

    new

--- src/modules/admin/Rekap/Rekap.jsx
+++ src/modules/admin/Rekap/Rekap.jsx
@@ -4,9 +4,9 @@ const Rekap = () => {
         <div className="bg-white w-[940px] h-[400px] rounded-[10px] ml-[300px] mt-28 flex p-8 ">
             <h2 className="font-poppins font-20 font-bold text-[#078DCC]">Rekap absen</h2>
             <div className="mt-[48px]">
-                <button className="bg-white font-poppins text-[16px] hover:border-[#DCE5F1]">
+                <button className="bg-white font-poppins text-[16px] ">
                     <h1>Kelas A</h1>
-                    <p></p>
+                    <p>English beginner</p>
                 </button>
             </div>
             
