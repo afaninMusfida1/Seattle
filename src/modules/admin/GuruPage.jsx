1da735f34f23c32e88fe9e5cfed597a8e995a9d6
afaninmusfida <afanin227089@student.smkn1kandeman.sch.id>
2024-04-20 12:22:38 +0700

    new

---
+++ src/modules/admin/GuruPage.jsx
@@ -0,0 +1,8 @@
+const GuruPage = () => {
+ return (
+    <>
+    <h1 className="text-center font-bold text-20 ">guru</h1>
+    <img src="src/assets/seattleLogo.png" alt="" />
+    </>
+ )}
+export default GuruPage
