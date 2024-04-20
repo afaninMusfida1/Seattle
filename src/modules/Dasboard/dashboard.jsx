c56aa18fe5d63e0bb87c70ab987a584c7f2af719
afaninmusfida <afanin227089@student.smkn1kandeman.sch.id>
2024-04-20 10:30:42 +0700

    update

--- src/modules/Dasboard/dashboard.jsx
+++ src/modules/Dasboard/dashboard.jsx
@@ -1,20 +1,20 @@
 function Dashboard() {
     return (
-        <div className="justify-self-center ml-[300px]">
+        <div className="justify-self-center mt-[100px] ml-[300px] max-w-screen max-h-screen">
 
-            <div className="flex justify-around pt-[8px]">
-                <div className=" bg-[#078DCC] size-[224px] w-[700px] rounded-[30px]">
+            <div className="flex justify-around pt-[8px] ">
+                <div className=" bg-[#078DCC] size-[220px] w-[500px] rounded-[30px]">
                     <h1 className="font-poppins text-[#FBFBFB] pt-28 text-4xl ml-7">Pengumuman</h1>
                     <p className="drop-shadow-xl font-poppins text-[#FFFFFF] text-lg pt-2 ml-7">Tidak ditemukan</p>
                 </div>
 
-                <div className=" bg-[#FFFFFF] size-[224px] w-[312px] rounded-[30px]">
+                <div className=" bg-[#FFFFFF] pt-[28px] mx-10 size-[220px] w-[312px] rounded-[30px]">
                 </div> 
             </div>
 
             <div className="flex justify-around pt-8">
+                <div className=" bg-[#FFFFFF] size-80 w-[400px] h-[400px] p-[50px] rounded-[30px]"></div>
                 <div className=" bg-[#FFFFFF] size-80 w-[400px] h-[400px] rounded-[30px]"></div>
-                <div className=" bg-[#FFFFFF] size-80 w-[600px] h-[400px] rounded-[30px]"></div>
             </div>
 
         </div>
