32dc4f4ad455b70ee5f3c5e2afbea3cb947fdfcc
ZelikaSheril <zelika227123@student.smkn1kandeman.sch.id>
2024-04-21 22:06:16 +0700

    revisi

--- src/modules/admin/Rekap/Rekap.jsx
+++ src/modules/admin/Rekap/Rekap.jsx
@@ -1,17 +1,107 @@
 const Rekap = () => {
 
-    return(
-        <div className="bg-white w-[940px] h-[400px] rounded-[10px] ml-[300px] mt-28 flex p-8 ">
-            <h2 className="font-poppins font-20 font-bold text-[#078DCC]">Rekap absen</h2>
-            <div className="mt-[48px]">
-                <button className="bg-white font-poppins text-[16px] ">
-                    <h1>Kelas A</h1>
-                    <p>English beginner</p>
-                </button>
+    return (
+        <div>
+            <div className="bg-white w-[940px] h-[400px] rounded-[10px] ml-[300px] mt-20 p-8 ">
+                <div>
+                    <h2 className="font-poppins font-20 font-bold text-[#078DCC]">Rekap Absen</h2>
+                </div>
+                <div className="mt-[20px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
+                <div className="mt-[10px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
+                <div className="mt-[10px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
             </div>
-            
-            
+
+            <div className="bg-white w-[940px] h-[400px] rounded-[10px] ml-[300px] mt-[50px] p-8 ">
+                <div>
+                    <h2 className="font-poppins font-20 font-bold text-[#078DCC]">Rekap Jurnal</h2>
+                </div>
+                <div className="mt-[20px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
+                <div className="mt-[10px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
+                <div className="mt-[10px]">
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas A</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas B</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                    <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] ml-[20px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                        <h1 className="font-bold text-[#6A6D76]">Kelas C</h1>
+                        <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                    </button>
+                </div>
+            </div>
+
+
         </div>
+
+
     )
 }
 export default Rekap;
