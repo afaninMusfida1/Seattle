dff009fc8aafcf4723db1096bf8afb40afa97a54
afaninmusfida <afanin227089@student.smkn1kandeman.sch.id>
2024-04-21 15:25:11 +0700

    new

--- src/modules/admin/TambahSiswa.jsx
+++ src/modules/admin/TambahSiswa.jsx
@@ -1,32 +1,40 @@
+import React, { useState } from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
+
 const TambahSiswa = () => {
+    const navigate = useNavigate();
+    const [showDafSiswa, setShowDafSiswa] = useState(false);
+
+    const daftarSiswa = () => {
+        setShowDafSiswa(true);
+    }
+
     return (
         <div className="bg-white w-[940px] h-[400px] rounded-[10px] ml-[300px] mt-28 flex p-8 ">
             <div>
                 <input placeholder="Nama" className="input w-[400px] h-[40px] font-poppins text-[16px] border-2 text-[#3F3F3F] bg-[#DCE5F1] rounded-[16px] outline-none hover:border-[#078DCC] "></input>
                 <select className="border rounded px-4 py-2 outline-none text-[#6A6D76] w-[400px] h-[40px] mb-[15px]">
-                    <option value="" >Jenis Kelamin</option>
+                    <option value="">Jenis Kelamin</option>
                     <option value="option1">Laki-Laki</option>
                     <option value="option2">Perempuan</option>
                 </select>
                 <input placeholder="No.telp" className="input w-[400px] h-[40px] font-poppins text-[16px] text-[#3F3F3F] border-2 bg-[#DCE5F1] rounded-[16px] outline-none hover:border-[#078DCC] "></input>
                 <select className="border rounded px-4 py-2 outline-none text-[#6A6D76] w-[200px] h-[40px] mb-[15px]">
-                    <option value="" >Kelas</option>
+                    <option value="">Kelas</option>
                     <option value="option1">English Pemula</option>
-                    <option value="option2">English Menengah </option>
+                    <option value="option2">English Menengah</option>
                     <option value="option3">English Sepuh</option>
                 </select>
-                <button className="w-[400px] h-[40px] mt-[80px] font-poppins text-[16px] border-2 bg-[#07CCB5] text-white rounded-[10px] outline-none ">Tambahkan</button>
+                <button onClick={daftarSiswa} className="w-[400px] h-[40px] mt-[80px] font-poppins text-[16px] border-2 bg-[#07CCB5] text-white rounded-[10px] outline-none ">Tambahkan</button>
+
             </div>
             <div>
                 <input placeholder="Email" className="input w-[400px] h-[40px] font-poppins text-[16px] text-[#3F3F3F] border-2 bg-[#DCE5F1] rounded-[16px] outline-none hover:border-[#078DCC] "></input>
                 <input placeholder="Asal Negara" className="input w-[400px] h-[40px] font-poppins text-[16px] text-[#3F3F3F] border-2 bg-[#DCE5F1] rounded-[16px] outline-none hover:border-[#078DCC] "></input>
-                <div className="py-[200px]">
-                <a href="" className="text-[#078DCC] text-[14px] ml-[300px] mt-[500px] hover:underline">Lihat data siswa</a>
-                </div>
+                <button onClick={() => navigate('/app/tambah-siswa/daftar-siswa')} className="text-[#078DCC] text-[14px] ml-[300px] mt-[210px] hover:underline">Lihat data siswa</button>
             </div>
-
+            <Outlet />
         </div>
-
     )
 }
 
--- src/modules/layout/Sidebar.jsx
+++ src/modules/layout/Sidebar.jsx
@@ -2,67 +2,77 @@ import Rekap from '../admin/Rekap/Rekap.jsx';
 import React, { useState } from 'react';
 import TambahGuru from '../admin/TambahGuru.jsx';
 import Dashboard from '../Dasboard/Dashboard.jsx';
+import { useNavigate } from 'react-router-dom';
+import TambahSiswa from '../admin/TambahSiswa.jsx';
+import DaftarGuru from '../admin/DaftarGuru.jsx';
 
 const Sidebar = () => {
-
+  const navigate = useNavigate();
   const [showDashboard, setShowDashboard] = useState(true);
   const [showPlusGuruPage, setShowPlusGuruPage] = useState(false);
-  // const [showSiswaPage, setShowSiswaPage] = useState(false);
-  // const [showRekapPage, setShowRekapPage] = useState(false);
+  const [showSiswaPage, setShowSiswaPage] = useState(false);
+  const [showRekapPage, setShowRekapPage] = useState(false);
+
 
   const navDashboard = () => {
+    navigate('/app/dashboard');
     setShowDashboard(true);
     setShowPlusGuruPage(false);
-    // setShowSiswaPage(false);
-    // setShowRekapPage(false);
+    setShowSiswaPage(false);
+    setShowRekapPage(false);
   }
 
-  const navGuruPage = () => {
+  const navTambahGuru = () => {
+    navigate('/app/tambah-guru');
+    <DaftarGuru/>;
     setShowDashboard(false);
     setShowPlusGuruPage(true);
-    // setShowSiswaPage(false);
-    // setShowRekapPage(false);
+    setShowSiswaPage(false);
+    setShowRekapPage(false);
   }
 
   const navSiswaPage = () => {
+    navigate('/app/tambah-siswa');
     setShowDashboard(false);
     setShowPlusGuruPage(false);
-    // setShowSiswaPage(true);
-    // setShowRekapPage(false);
+    setShowSiswaPage(true);
+    setShowRekapPage(false);
   }
 
   const navRekapPage = () => {
+    navigate('/app/rekap');
     setShowDashboard(false);
     setShowPlusGuruPage(false);
-    // setShowSiswaPage(false);
-    // setShowRekapPage(true);
+    setShowSiswaPage(false);
+    setShowRekapPage(true);
   }
 
-  const logout = () => {
-    console.log ("logout")
-  }
-  return (
-    <div className="flex h-screen bg-gray-200 ">
-      <div className="fixed bg-white text-white w-64 h-screen">
-        <div className="flex items-center justify-between p-4">
-          <img src="src/assets/seattleLogo.png" alt="Logo" className="mx-[25px] h-[60px] w-[120px]" />
-        </div>
-        <nav className="mt-4 px-[25px]">
-          <section className="text-sm block px-4 py-2 font-bold text-[#A7B9D1]">Overview</section>
-          <button onClick={navDashboard} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Dashboard</button>
-          <button onClick={navGuruPage} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Guru</button>
-          <button onClick={navSiswaPage} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Siswa</button>
-          <button onClick={navRekapPage} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Rekap</button>
-          <button onClick={logout} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white my-[240px] text-left">Log Out</button>
-        </nav>
+const logout = () => {
+  console.log("logout")
+  navigate('/login');
+}
+
+return (
+  <div className="bg-gray-200 flex">
+    <div className="fixed bg-white text-white w-64 h-screen">
+      <div className="flex items-center justify-between p-4">
+        <img src="src/assets/seattleLogo.png" className="mx-[25px] h-[60px] w-[120px]" />
       </div>
-      {showDashboard && <Dashboard />}
-      {showPlusGuruPage && <TambahGuru/>}
-      
-      {/* {showSiswaPage && <SiswaPage />}
-      {showRekapPage && <RekapPage />} */}
+      <nav className="mt-4 px-[25px]">
+        <section className="text-sm block px-4 py-2 font-bold text-[#A7B9D1]">Overview</section>
+        <button onClick={navDashboard} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Dashboard</button>
+        <button onClick={navTambahGuru} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Guru</button>
+        <button onClick={navSiswaPage} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Siswa</button>
+        <button onClick={navRekapPage} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white text-left">Rekap</button>
+        <button onClick={logout} className="bg-white w-[200px] hover:bg-[#078DCC] hover:rounded-[5px]  block px-4 py-2 text-sm text-black hover:text-white my-[240px] text-left">Log Out</button>
+      </nav>
     </div>
-  );
+    {showDashboard }
+    {showPlusGuruPage }
+    {showSiswaPage}
+    {showRekapPage}
+  </div>
+);
 }
 
 
