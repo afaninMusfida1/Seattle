2cb2309cd5e1b39377647ab5cf2649e1a74d416b
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-05-12 18:00:34 +0700

    menambahkan kelas item

---
+++ src/modules/kelas/KelasItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const KelasItem = () => {
+    return (
+        <>
+            <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
+                <h1 className="font-bold text-[#6A6D76]">Kelas Item</h1>
+                <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+            </button>
+        </>
+    )
+}
+
+export default KelasItem
