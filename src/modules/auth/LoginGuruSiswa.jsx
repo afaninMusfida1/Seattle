ca879ba3fd4d71a6dc4dda454296c90569bb8b05
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-05-12 21:43:15 +0700

    fixing tampilan login siswa

--- src/modules/auth/LoginGuruSiswa.jsx
+++ src/modules/auth/LoginGuruSiswa.jsx
@@ -14,14 +14,14 @@ const LoginGuruSiswa = () => {
 
   return (
     <>
-      <div className="bg-[#FBFBFB] h-full min-h-screen">
+      <div className="bg-[#FBFBFB] w-screen h-screen">
         <div className="logo">
-          <img className="mx-10 pt-5 w-30 h-20" src="src/assets/seattleLogo.png" alt="" />
+          <img className="mx-10 pt-5 w-auto h-20" src="src/assets/logo - Copy.png" alt="" />
         </div>
         <h1 className="text-center text-[#06357A] font-poppins font-bold md:text-[40px] text-[20px] p-5 mt-[50px] my-[25px]">Masuk sebagai</h1>
-        <div className="container flex flex-col">
-          <button onClick={handleClickGuru} className="bg-[#078DCC] text-center font-poppins text-[#fbfbfb] md:text-[20px] text-[16px] rounded-[16px] px-5 py-3 mt-[35px]" >Guru</button>
-          <button onClick={handleClickSiswa} className="bg-[#078DCC] text-center font-poppins text-[#fbfbfb] md:text-[20px] text-[16px] rounded-[16px] px-5 py-3 mt-[35px]" >Siswa</button>
+        <div className=" flex-col max-w-fit mx-auto">
+          <button onClick={handleClickGuru} className="bg-[#078DCC] text-center block w-full px-[180px] my-6 font-poppins text-[#fbfbfb] md:text-[20px] text-[16px] rounded-[16px] py-3 " >Guru</button>
+          <button onClick={handleClickSiswa} className="bg-[#078DCC] text-center block w-full px-[180px] my-6 font-poppins text-[#fbfbfb] md:text-[20px] text-[16px] rounded-[16px] py-3" >Siswa</button>
         </div>
       </div>
     </>
--- src/modules/kelas/KelasItem.jsx
+++ src/modules/kelas/KelasItem.jsx
@@ -5,7 +5,7 @@ const KelasItem = () => {
         <>
             <button className="bg-white font-poppins text-[16px] text-left border-2 py-2 pr-[140px] pl-[15px] rounded-[10px] hover:bg-[#DCE5F1] hover:border-[#078DCC]">
                 <h1 className="font-bold text-[#6A6D76]">Kelas Item</h1>
-                <p className="text-[#6A6D76] mt-[10px]">English beginner</p>
+                <h1 className="text-[#6A6D76] mt-[10px]">English beginner</h1>
             </button>
         </>
     )
