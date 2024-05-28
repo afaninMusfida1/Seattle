5c7855673384cb985a47335a88ba920aa483524d
Chalimatussadiyyah <chalimatus227096@student.smkn1kandeman.sch.id>
2024-05-28 09:02:29 +0700

    neww

--- src/modules/admin/crud-siswa/SiswaProvider.jsx
+++ src/modules/admin/crud-siswa/SiswaProvider.jsx
@@ -1,16 +1,14 @@
 import React, { createContext, useContext, useState } from 'react';
-import axios from 'axios';
-import { API_URL } from '../../config/Url';
-import {addSiswa, apiGetSiswa } from './RequestSiswa';
+import { addSiswa, apiGetSiswa, deleteSiswa, editSiswa } from './RequestSiswa';
 
 const initSiswaState = {
-    siswaList: [], 
+    siswaList: [],
     isLoading: false,
     handleAdd: () => {},
     handleUpdate: () => {},
     handleDelete: () => {},
     handleFetch: () => {},
-}
+};
 
 const SiswaContext = createContext(initSiswaState);
 export const useSiswa = () => useContext(SiswaContext);
@@ -19,74 +17,68 @@ export const SiswaProvider = ({ children }) => {
     const [siswaList, setSiswaList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-
-    const handleFetch = async () => {
-        const data = await apiGetSiswa();
-        setSiswaList(data); 
+    const handleFetch = () => {
+        setIsLoading(true);
+        apiGetSiswa()
+        .then(data => {
+            setSiswaList(Array.isArray(data) ? data : []);
+            setIsLoading(false);
+        });
     };
 
-    const handleAdd = async (nama, kelas_id, level, no_telp_ortu, email, password) => {
+    const handleAdd = (nama, kelas_id, no_telp_ortu, email, password) => {
         if (isLoading) return;
     
         setIsLoading(true);
     
-        const apiCall = await addSiswa(nama, kelas_id, level, no_telp_ortu, email, password)
-        setIsLoading(false)
-
-        return apiCall
+        addSiswa(nama, kelas_id, no_telp_ortu, email, password)
+        .then(apiCall => {
+            if (apiCall && apiCall.success) { 
+                setSiswaList(prevSiswaList => [...prevSiswaList, apiCall.data]);
+                alert("Siswa ditambahkan");
+            } else {
+                alert("Gagal menambahkan siswa. Terjadi kesalahan.");
+            }
+            setIsLoading(false);
+        })
+        .catch(error => {
+            alert("Gagal menambahkan siswa. Terjadi kesalahan: " + error);
+            setIsLoading(false);
+        });
     };
     
 
     const handleDelete = (id) => {
+        if (isLoading) return;
+        setIsLoading(true);
 
-        if (isLoading) return
-        setIsLoading(true)
-
-        const token = localStorage.getItem('adminToken');
-        if (!token) {
-            console.error("Token not found. Please login again.");
-            return;
-        }
-
-        axios.delete(`${API_URL}/siswa/${id}`, {
-            headers: {
-                Authorization:` Bearer ${token}`
+        deleteSiswa(id)
+        .then(response => {
+            if (response.status === 200) {
+                setSiswaList(prevList => prevList.filter(siswa => siswa.id !== id));
+                alert("Berhasil Mendelete");
+            } else {
+                alert("Gagal mendelete siswa");
             }
-        })
-        .then(() => {
-            setSiswaList(prevList => prevList.filter(siswa => siswa.id !== id));
-            setIsLoading(false)
-            alert("Berhasil Mendelete");
-        })
-        .catch(error => {
-            console.error("Error deleting guru:", error.response ? error.response.data : error.message);
-            setIsLoading(false)
-            alert("Gagal mendelete guru");
+            setIsLoading(false);
         });
-        
     };
 
-    const handleUpdate = async (id, updatedData) => {
-        const token = localStorage.getItem('adminToken');
-        if (!token) {
-            console.error("Token not found. Please login again.");
-            return;
-        }
+    const handleUpdate = (id, updatedDataSiswa) => {
+        if (isLoading) return;
+        setIsLoading(true);
 
-        axios.put(`${API_URL}/siswa/${id}`, updatedData, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        editSiswa(id, updatedDataSiswa)
         .then(response => {
-            setSiswaList(prevList => prevList.map(siswa => 
-                siswa.id === id ? { ...siswa, ...updatedData } : siswa
-            ));
-            alert("Berhasil Mengupdate");
-        })
-        .catch(error => {
-            console.error("Error updating guru:", error.response ? error.response.data : error.message);
-            alert("Gagal mengupdate siswa");
+            if (response.status === 200) {
+                setSiswaList(prevList => prevList.map(siswa => 
+                    siswa.id === id ? { ...siswa, ...updatedDataSiswa } : siswa
+                ));
+                alert("Berhasil Mengupdate");
+            } else {
+                alert("Gagal mengupdate siswa");
+            }
+            setIsLoading(false);
         });
     };
 
@@ -96,5 +88,3 @@ export const SiswaProvider = ({ children }) => {
         </SiswaContext.Provider>
     );
 };
-
-
