5fe30ed8ad2f2b445c12027f5ec3d3f1479de7ef
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-05-29 15:03:40 +0700

    file rekapguru & jurnal absen

---
+++ src/modules/gurug/crud-jurnal/RekapGuru.jsx
@@ -0,0 +1,41 @@
+import { useNavigate, Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLayout } from '../../layout/LayoutContext';
+// import { useKelas } from './crud-presensi/PresensiProvider';
+import KelasGrouping from '../../admin/crud-kelas/KelasGrouping';
+import KelasItem from '../../admin/crud-kelas/KelasItem';
+import { useKelas } from '../../admin/crud-kelas/KelasProvider';
+import { useJurnal } from './JurnalProvider';
+
+const Rekap = () => {
+    const navigate = useNavigate();
+    const { actionSetPageTitle } = useLayout()
+    const { daftarKelas, location, setLocation, handleFetchKelas } = useJurnal()
+
+    useEffect(() => {
+        actionSetPageTitle('Lihat Rekap')
+        handleFetchKelas()
+    }, [])
+
+    // function handleChangeAbsen() {
+    //     navigate('/guru/rekap/kbm')
+    // }
+
+    const groupedKelas = daftarKelas.length == 0 ? [] :
+        Object.groupBy(daftarKelas, ({ kategori }) => kategori)
+
+    console.log(groupedKelas)
+
+    return (
+        <div className="mt-[50px] ">
+
+            {
+                Object.entries(groupedKelas).map(
+                    (value, key) =>
+                        <KelasGrouping key={key} location={location} kategori={value[0]} />)
+            }
+
+        </div>
+    );
+};
+export default Rekap;
