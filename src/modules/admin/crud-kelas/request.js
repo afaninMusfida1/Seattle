d06c32960938be4c8e81e22d07ad3c7dfdea3f11
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-05-30 04:05:23 +0700

    fixing cek jurnal

--- src/modules/admin/crud-kelas/request.js
+++ src/modules/admin/crud-kelas/request.js
@@ -28,7 +28,7 @@ export const addKelas = async (nama_kelas, kategori, periode, jadwal_kelas) => {
 
 export const apiGetKelas = async (role) => {
   const token = role == 'Admin' ? getToken() : getTokenGuru()
-  console.log(role)
+  console.log(`role: `, role)
   if (!token) {
     console.error("Token not found. Please login again.");
     return { message: "Token not found. Please login again." };
