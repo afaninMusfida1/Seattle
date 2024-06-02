5df2ba480c50d978cb46a848d4ffda665a4d33c1
GadangAkbar <gadang227104@student.smkn1kandeman.sch.id>
2024-06-02 15:56:57 +0700

    fixing jurnal admin & daftar siswa

--- src/modules/gurug/crud-presensi/PresensiItem.jsx
+++ src/modules/gurug/crud-presensi/PresensiItem.jsx
@@ -3,24 +3,20 @@ import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 
-const PresensiItem = ({ siswa, handleAddPresensi, doPresensi }) => {
+const PresensiItem = ({ siswa, handleAddPresensi }) => {
 
     const handlePresensiChange = (e) => {
         const keterangan = e.target.value;
         handleAddPresensi(siswa.id, keterangan);
     };
 
-    return (z
+    return (
         <div className="item-container flex justify-between w-full border-2 rounded-lg px-4 py-1">
             <div className="nama">{siswa.nama}</div>
-            {doPresensi ? (
-                <>
                     <div>
                         <FontAwesomeIcon icon={faSquareCheck} />   
                     </div>
-                </>
-            ):(
-                <>
+
                 <div className="status flex gap-6">
                 <input
                     type="radio"
@@ -47,9 +43,6 @@ const PresensiItem = ({ siswa, handleAddPresensi, doPresensi }) => {
                 />
                 <label htmlFor="sakit">S</label><br />
             </div>
-                </>
-            ) }
-
         </div>
     );
 };
